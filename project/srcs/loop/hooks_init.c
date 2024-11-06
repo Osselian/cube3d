2745eb6a86e42d5664ae3ed911f4da86591adff7
Osselian <rossemir11@gmail.com>
2024-11-06 20:39:28 +0400

    cross exit

--- project/srcs/loop/hooks_init.c
+++ project/srcs/loop/hooks_init.c
@@ -6,7 +6,7 @@
 /*   By: defimova <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/06 17:27:30 by defimova          #+#    #+#             */
-/*   Updated: 2024/11/06 17:27:33 by defimova         ###   ########.fr       */
+/*   Updated: 2024/11/06 17:27:33y sodefimova       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,7 @@ int	hooks_init(t_mlx *wm, t_data *g)
 {
 	if (!wm || !wm->win || !g)
 		return (1);
-	if (mlx_hook(wm->win, ON_KEYDOWN, KEY_PRESS_MASK, on_key_pressed, g))
-		return (1);
-	if (mlx_hook(wm->win, ON_DESTROY, DESTROY_MASK, exit_game, g))
-		return (1);
+	mlx_hook(wm->win, ON_KEYDOWN, KEY_PRESS_MASK, on_key_pressed, g);
+	mlx_hook(wm->win, ON_DESTROY, DESTROY_MASK, exit_game, g);
 	return (0);
 }
--- project/srcs/main.c
+++ project/srcs/main.c
@@ -30,7 +30,6 @@ int	main(int argc, char **argv)
 	hooks_init(&game->win_mng, game);
 	draw_frame(game);
 	mlx_loop(game->win_mng.mlx);
-	mlx_loop_hook(game->win_mng.mlx, on_key_pressed, game);
 	free_meta(metadata);
 	free_data(game);
 	return (0);
