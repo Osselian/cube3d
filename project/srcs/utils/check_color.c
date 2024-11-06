4c8eea5387fda23f12c7c02f459da3c12866a4e2
Osselian <rossemir11@gmail.com>
2024-11-06 17:33:14 +0400

    fix color check

--- project/srcs/utils/check_color.c
+++ project/srcs/utils/check_color.c
@@ -1,26 +1,28 @@
 #include "../../incs/cub3D.h"
 
+static bool	return_err(char *ln)
+{
+	print_error(INCORRECT_COLOR);
+	free(ln);
+	return (true);
+}
+
 bool	check_color(char *ln)
 {
 	int	i;
 
 	i = 0;
+	printf("Color: %s\n", ln);
+	if (ft_strlen(ln) == 0)
+		return (return_err(ln));
 	while (ln[i] != '\0')
 	{
 		if (!ft_isdigit(ln[i]))
-		{
-			print_error(INCORRECT_COLOR);
-			free(ln);
-			return (true);
-		}
+			return (return_err(ln));
 		i++;
 	}
 	if (ft_atoi(ln) < 0 || ft_atoi(ln) > 255)
-	{
-		print_error(INCORRECT_COLOR);
-		free(ln);
-		return (true);
-	}
+		return (return_err(ln));
 	free(ln);
 	return (false);
 }
